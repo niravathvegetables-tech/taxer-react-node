411421f1d07c7d4d63ad3edcdde599715b6e1819
niravathvegetables-tech <niravathvegetables@gmail.com>
2026-05-28 22:49:26 +0530

    Add start script

--- nodeproject/taxer/db.js
+++ nodeproject/taxer/db.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql2");
 
+// const db = mysql.createConnection({
+//   host: "db.fr-pari1.bengt.wasmernet.com",
+//   user: "82703f9270c580001276041e65bc",
+//   password: "069b8270-3f92-71d7-8000-c190145b200e",
+//   database: "dbYxy8VbuUDQmdA53q6Vhac8"
+// });
+
 const db = mysql.createConnection({
-  host: "db.fr-pari1.bengt.wasmernet.com",
-  user: "82703f9270c580001276041e65bc",
-  password: "069b8270-3f92-71d7-8000-c190145b200e",
-  database: "dbYxy8VbuUDQmdA53q6Vhac8"
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "taxer-react-node"
 });
 
 db.connect((err) => {

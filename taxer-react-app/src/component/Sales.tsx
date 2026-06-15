81627fc24a2cdfc258752ed74f5ab3ac7993c321
niravathvegetables-tech <niravathvegetables@gmail.com>
2026-06-15 21:39:20 +0530

    Sales modal started

--- taxer-react-app/src/component/Sales.tsx
+++ taxer-react-app/src/component/Sales.tsx
@@ -28,12 +28,70 @@ interface Stock{
   }
 
 
+  interface FormData {
+ 
+  company_id: string;
+  tax_id:number | null;
+  date:string | null;
+}
+
+
+
 
 const Sales: React.FC<SalesProps> = ({companyid,taxidee,taxarray,stocks}) => {
+
+  const [formData, setFormData] = useState<FormData>({
+  company_id: companyid ?? "",
+  tax_id: taxidee,
+  date: new Date().toISOString().split("T")[0], // ✅ added
+});
+
+
+
+const [showModal, SetshowModal]=useState<boolean>(false);
+
+const handleShowModal = () => {
+  SetshowModal(true);
+};
+
+
+const cancelModal = () =>{
+
+   SetshowModal(false);
+};
+
+
   return (
     <div className="contra-container">
       <h1>Sales</h1>
       <p>This is the Sales.</p>
+       <a className="default-btn" onClick={handleShowModal}>Add Sales</a>
+
+       {showModal &&(
+
+
+        <div className="modal-overlay">
+          <div className="modal-box modalpos">
+               <label>Company Id</label>
+              <input
+                name="company_id"
+                value={formData.company_id}
+                readOnly
+              />
+
+               <label>Tax</label>
+
+               
+
+                <button className="btn-submit"  > Submit  </button>
+              
+              <button className="btn-cancel" onClick={cancelModal} >  Cancel  </button>
+
+            </div>
+            </div>
+
+        )};
+
     </div>
   );
 }   
